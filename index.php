c1333880f771e10555957e5cb9e65d72054d8b5c
ulyan <ulyana.glushchenko2031@mail.ru>
2025-02-15 22:29:18 +0300

    HW 1

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style_default.css">
+    <title>HW 1</title>
+</head>
+
+<body>
+    <header>
+        <img src="images/mospolytech_logo.png">
+        <h1>Домашняя работа: Hello, World!</h1>
+    </header>
+
+    <main>
+        <h2><?php echo "Hello, World!"; ?></h2>
+    </main>
+
+    <footer>
+        <p>Создать веб-страницу с динамическим контентом. Загрузить код в удаленный репозиторий. </p>
+    </footer>
+</body>
+
+</html>
