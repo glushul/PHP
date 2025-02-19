7809c05d5a8cb8f81018a13c8c61ac3e3342caba
ulyan <ulyana.glushchenko2031@mail.ru>
2025-02-19 13:48:32 +0300

    HW 2

--- index.php
+++ index.php
@@ -4,23 +4,31 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/style_default.css">
-    <title>HW 1</title>
+    <link rel="stylesheet" href="css/style.css">
+    <title>Серверная разработка</title>
 </head>
 
 <body>
     <header>
-        <img src="images/mospolytech_logo.png">
-        <h1>Домашняя работа: Hello, World!</h1>
+        <img src="images/mospolytech_logo.png"  alt="Лого">
+        <h1>Глущенко Ульяна</h1>
     </header>
 
     <main>
-        <h2><?php echo "Hello, World!"; ?></h2>
+        <h2>Домашние работы</h2>
+        <ol>
+            <li><a href="hws/hw1.php">Hello, World!</a></li>
+            <li><a href="hws/hw2.php">Feedback Form</a></li>
+        </ol>
+        <h2>Лабораторные работы</h2>
+        <ol>
+            <li><a href="#">Нет</a></li>
+        </ol>
+        <h2>Проект</h2>
+        <ol>
+            <li><a href="#">Нет</a></li>
+        </ol>
     </main>
-
-    <footer>
-        <p>Создать веб-страницу с динамическим контентом. Загрузить код в удаленный репозиторий. </p>
-    </footer>
 </body>
 
 </html>
